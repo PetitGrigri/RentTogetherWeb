b9dd7603cf5b746b63383416f74ced26cc147b64
PetitGrigri <fgriselles@hotmail.fr>
2018-03-28 00:30:22 +0200

    Intégration Router

---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+
+    render() {
+        return (
+            <div className="row">
+                <h3>Hello</h3>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
